6e31e89638a70dd9afc2a6cf97e13c3a8f9eca63
NickKline9931 <nickkline9931@gmail.com>
2023-03-31 08:31:32 -0400

    Fix bug where search results generated empty pages

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ export default function App() {
     const data = await fetch(
       "https://api.artic.edu/api/v1/artworks/search?q=" +
         query +
-        "&fields=title,artist_display,date_display,term_titles,image_id&query[term][is_public_domain]=true&page=1&limit=10",
+        "&fields=title,artist_display,date_display,term_titles,image_id&query[term][is_public_domain]=true&page=1&limit=12",
       {
         headers: {
           "AIC-User-Agent": "Art-Gallery (nickkline9931@gmail.com)",
